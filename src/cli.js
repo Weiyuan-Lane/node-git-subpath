0afb7c15def68881f421c8160f772356a5387d1b
Weiyuan <weiyuan.lane@gmail.com>
2019-04-22 00:56:24 +0800

    [Ver] Bumped version to 0.0.4

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@
 
 const program = require('commander')
 program
-  .version('0.0.3')
+  .version('0.0.4')
   .option('-p, --path <value>', 'Target path to submit')
   .option('-m, --module <value>', 'Module name')
   .option('-t, --tag <value>', 'Tag version of module')
