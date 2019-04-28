7f0e25564974ca6eb294be4ebb187ba09cbafccb
Weiyuan <weiyuan.lane@gmail.com>
2019-04-28 23:40:03 +0800

    [Ops] Add .gitignore and remove node_modules from repo

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@
 
 const program = require('commander')
 program
-  .version('0.0.6')
+  .version('0.0.7')
   .option('-p, --path <value>', 'Target path to submit')
   .option('-m, --module <value>', 'Module name')
   .option('-t, --tag <value>', 'Tag version of module')
